eb62ef19ae8b9113e1b8a55f02a3e846562d62b2
wScottSh <7772230+wScottSh@users.noreply.github.com>
2018-02-05 11:17:18 -0600

    initial commit

---
+++ src/fire.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyDwhgUbIueZ_ejPan5VrROV3y3qomzdmRU",
+    authDomain: "gatekeepers-ccg.firebaseapp.com",
+    databaseURL: "https://gatekeepers-ccg.firebaseio.com",
+    projectId: "gatekeepers-ccg",
+    storageBucket: "",
+    messagingSenderId: "817348093630"
+  };
