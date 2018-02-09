ee8668845d88cfe80e2a3c63efa364e179dfe34b
wScottSh <7772230+wScottSh@users.noreply.github.com>
2018-02-08 19:52:44 -0600

    simplified the front page to only contain the header and the global deck

--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react'
 import './CardsContainer.css'
-import CardCreator from '../CardCreator/CardCreator'
 import GlobalDeck from '../GlobalDeck/GlobalDeck'
 
 class CardsContainer extends Component {
   render() {
     return (
       <div className="cardsContainer">
-        <h1>Cards Container</h1>
-        <CardCreator />
         <GlobalDeck />
       </div>
     )
